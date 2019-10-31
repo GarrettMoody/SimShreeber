621506e6f587099110ff758a507c5d153922a193
Garrett Moody <garrett.moody@snc.edu>
2019-10-30 20:26:37 -0500

    Added Cooldown Bars
    - Added cooldown bars to the cheese maker

---
+++ Assets/Scripts/AlwaysFaceCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysFaceCamera : MonoBehaviour
+{ 
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(2 * this.transform.position - Camera.main.transform.position);
+    }
+}
--- Assets/Scripts/CheeseMaker.cs
+++ Assets/Scripts/CheeseMaker.cs
@@ -1,19 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CheeseMaker : MonoBehaviour
 {
     public ObjectInstantiator objectInstantiator;
+    public float cooldownTime;
+    public Slider cooldownSlider;
+
+    private float currentCooldownValue;
+    private bool isCooling; 
+
+    public void Update()
+    {
+        if(isCooling)
+        {
+            currentCooldownValue += Time.deltaTime;
+            cooldownSlider.value = currentCooldownValue;
+            if(currentCooldownValue >= cooldownTime)
+            {
+                isCooling = false;
+                cooldownSlider.gameObject.SetActive(false);
+            }
+        }
+    }
 
     public GameObject MakeCheese()
     {
-        return objectInstantiator.InstantiateObject();
+        if(!isCooling)
+        {
+            StartCooling();
+            return objectInstantiator.InstantiateObject();
+        } else
+        {
+            return null;
+        }
     }
 
     public void OnMouseDown()
     {
         GameObject newCheese = MakeCheese();
-        newCheese.GetComponent<Rigidbody>().isKinematic = false;
+        if(newCheese != null)
+        {
+            newCheese.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+
+    private void StartCooling()
+    {
+        isCooling = true;
+        currentCooldownValue = 0f;
+        cooldownSlider.gameObject.SetActive(true);
     }
 }
